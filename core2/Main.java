8faced6344f26e842c28086efb32e344e65d794d
JuliaKodosova <47756834+JuliaKodosova@users.noreply.github.com>
2019-03-08 23:40:52 +0300

    task13 (#11)

---
+++ core2/Main.java
@@ -0,0 +1,16 @@
+package core2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Massive massive = new Massive();
+		massive.method1();
+		System.out.println("------------");
+		massive.method2();
+		System.out.println("------------");
+		massive.method3();
+		System.out.println("------------");
+		massive.method4();
+	}
+}
